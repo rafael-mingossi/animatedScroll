f952743d0bf2fd4112ec89246f5cbccf7f2628c4
rafael-mingossi <rafaelmingossi@gmail.com>
2023-02-06 10:01:38 +1100

    adding first screen

---
+++ scr/screens/Post/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  title: {
+    fontSize: 32,
+    color: "#FFF",
+    fontWeight: "bold",
+  },
+  text: {
+    color: "#FFF",
+    fontSize: 18,
+    marginVertical: 22,
+  },
+});
